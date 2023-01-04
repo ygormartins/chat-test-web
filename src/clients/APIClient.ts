90fe1fe3f20d4da39030c6312dbd6d6c459b2def
ygormartins <ygormartinsr@outlook.com>
2023-01-03 23:42:22 -0300

    chore: adding the idToken to requests

--- src/clients/APIClient.ts
+++ src/clients/APIClient.ts
@@ -1,6 +1,21 @@
 /*---------- External ----------*/
+import { getAccessToken } from "@/services/AuthService";
 import Axios from "axios";
 
 export const APIClient = Axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+});
+
+APIClient.interceptors.request.use(async (config) => {
+  if (config.headers) {
+    const idToken = await getAccessToken();
+
+    if (!idToken) {
+      throw new Error("Unauthenticated");
+    }
+
+    config.headers.Authorization = `Bearer ${idToken}`;
+  }
+
+  return config;
 });
